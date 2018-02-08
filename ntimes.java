3d66a5171007cc0f09c5ac6a293bc9942e1ce797
Navaneethavishnu <31511355+Navaneethavishnu@users.noreply.github.com>
2018-02-08 16:45:58 +0530

    Create ntimes.java

---
+++ ntimes.java
@@ -0,0 +1,17 @@
+package navanee;
+
+import java.util.Scanner;
+
+public class ntimes {
+	public static void main(String[] args) {
+		Scanner s =new Scanner(System.in);
+		System.out.println("Enter the Number");
+		int n=s.nextInt();
+		int i=1;
+		while(i<=n){
+			System.out.println("Hello");
+			i++;
+		}
+		s.close();
+	}		
+}
