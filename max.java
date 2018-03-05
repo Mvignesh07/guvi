acca98f8bcb02b1be87bb17b5c142cc5706ab856
Navaneethavishnu <31511355+Navaneethavishnu@users.noreply.github.com>
2018-03-05 18:19:03 +0530

    Create max.java

---
+++ max.java
@@ -0,0 +1,24 @@
+package guvi;
+import java.util.*;
+public class max {
+     public static void main(String[] args) {
+    	 int n,max;
+    	 Scanner s=new Scanner(System.in);
+    	 System.out.println("enter the given number");
+    	 n=s.nextInt();
+    	 int a[]= new int[n];
+    	 System.out.println("enter the elment");
+    	 for(int i=0;i<n;i++) {
+    		 a[i]=s.nextInt();
+    		 
+    	 }
+    	 max=a[0];
+    	 for(int i=0;i<n;i++) {
+    		 if(max<a[i]) {
+    			 max=a[i];
+    		 }
+    	 }
+    	 System.out.println("The largest number is"+max);
+    	 s.close();
+     }
+}
