18f3c099dc714da8faddfbe50c3ec9d0110c5645
Navaneethavishnu <31511355+Navaneethavishnu@users.noreply.github.com>
2018-03-13 18:18:58 +0530

    Create mintohr.java

---
+++ mintohr.java
@@ -0,0 +1,14 @@
+package guvi;
+import java.util.*;
+public class mintohr {
+    public static void main(String[] args) {
+	 Scanner s=new Scanner(System.in);
+	 float m2;
+	 System.out.println("enter minute");
+	 float m1=s.nextFloat();
+     m2=m1/60;
+	 System.out.println("time is "+m2);
+     s.close();
+     
+    }
+}
