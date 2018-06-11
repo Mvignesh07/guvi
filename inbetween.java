10c6a4a25055969b6d53c0cb3333a3dac4a475aa
Navaneethavishnu <31511355+Navaneethavishnu@users.noreply.github.com>
2018-06-11 15:31:53 +0530

    Create inbetween.java

---
+++ inbetween.java
@@ -0,0 +1,17 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class inbetween {
+ public static void main(String[] args) {
+	Scanner sacn=new Scanner(System.in);
+	int s1=sacn.nextInt();
+		if(s1<=9) {
+		System.out.println("yes");
+	}else {
+		System.out.println("no");
+	}
+		sacn.close();
+	}
+
+}
