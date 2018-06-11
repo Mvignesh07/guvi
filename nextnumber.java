4e3aacdb1db9bf714f6ac092ebccf65e03735679
Navaneethavishnu <31511355+Navaneethavishnu@users.noreply.github.com>
2018-06-11 15:47:31 +0530

    Create nextnumber.java

---
+++ nextnumber.java
@@ -0,0 +1,10 @@
+package guvi;
+import java.util.*;
+public class nextnumber {
+public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+	int s1=s.nextInt();
+	System.out.println("the next number"+(s1+1));
+s.close();
+}
+}
