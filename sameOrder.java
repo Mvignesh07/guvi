f788d389f7cb54506640af6102db925b51b680c6
Navaneethavishnu <31511355+Navaneethavishnu@users.noreply.github.com>
2018-08-10 17:31:54 +0530

    Create sameOrder.java

---
+++ sameOrder.java
@@ -0,0 +1,15 @@
+package set6;
+import java.util.*;
+public class sameOrder {
+	 public static void main(String[] args){
+  	   Scanner s=new Scanner(System.in);
+  	     int b=s.nextInt();
+  	     String a=String.valueOf(b);
+  	     char c[]=a.toCharArray();
+  	     for(int i=0;i<c.length;i++)
+  	     {
+  	         System.out.printf("%c \t",c[i]);
+  	     }
+   }
+ 
+}
